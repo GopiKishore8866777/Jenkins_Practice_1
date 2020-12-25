5b960d078e5fde01d3898514c6ea12e561e99559
admin <gopikishore8866777@gmail.com>
2020-12-25 13:11:08 +0530

    added 2 new methods

---
+++ src/test/java/testPaks/Basic3.java
@@ -0,0 +1,16 @@
+package testPaks;
+
+import org.testng.annotations.Test;
+
+public class Basic3 {
+
+	@Test
+	public void m5() {
+		System.out.println("This is m5 method");
+	}
+	
+	@Test
+	public void m6() {
+		System.out.println("This is m6 method");
+	}
+}
